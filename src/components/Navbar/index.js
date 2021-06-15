88d34751ac3acc086bac67dcb9fdfa8a6ee1c2b4
Buddy Harlow <catlinharlow1758@gmail.com>
2021-06-15 08:41:52 -0400

    Navbar and styled components complete

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import { 
+    Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks,
+    NavBtn,
+    NavBtnLink 
+ } from './NavbarElements'
+
+const Navbar = ({toggle}) => {
+    return (
+        <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to="/">
+                    Mix Buddy
+                </NavLogo>
+                <MobileIcon onClick={toggle}>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="about">About</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="discover">Discover</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="signup">Sign Up</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                </NavBtn>
+            </NavbarContainer>
+        </Nav>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+import SideBar from '../components/SideBar'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <>
+            <SideBar isOpen={isOpen} toggle={toggle}/>
+            <Navbar toggle={toggle}/>
+        </>
+    )
+}
+
+export default Home
